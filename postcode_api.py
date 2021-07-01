618a7c949f31038fb28a111d9893be53556620b1
Niki <nnikiforidi@spartaglobal.com>
2021-07-01 15:02:35 +0100

    Going through lecture recordings and adding info to code and readme

--- postcode_api.py
+++ postcode_api.py
@@ -1,25 +1,32 @@
-# We already touched base on API using requests
-import requests
+# ### Postcode with API
 
-
-
-# First iteration
-#check_response_postcode = requests.get("https://api.postcodes.io/postcodes/wc1h8jz")
+# - We already touched base on API using requests
+# - In Terminal: pip install requests
+# import requests
+#
+# # - First iteration
+#
+# check_response_postcode = requests.get("https://api.postcodes.io/postcodes/wc1h8jz")
+# print(check_response_postcode.status_code)   # To check the status
+#
+#
 # if check_response_postcode.status_code == 200:
 #
 #     print(f"The status code is: {check_response_postcode.status_code}")
 #
 # else :
-#     print("Something went wrong")
+#     print("Something went wrong, try another postcode")
+#
+#
 
 
+# - Second iteration
+# **NOTE** in this iteration there is no .status_code when checking the response, nor any condition ( == 200 )
 
-
-# Second iteration
-
-
-#Research how request is doing tha behind the scene
-#check_response_postcode = requests.get("https://api.postcodes.io/postcodes/wc1h8jz")
+# import requests
+#
+# check_response_postcode = requests.get("https://api.postcodes.io/postcodes/wc1h8jz")
+#
 # def checking_status_code():
 #     if check_response_postcode:  # all the code and functionality is abstracted from the user.
 #      return(f"Success: {check_response_postcode}")
@@ -31,22 +38,32 @@ import requests
 
 
 # Third iteration
-# check_response_postcode = requests.get("https://api.postcodes.io/postcodes/wc1h8jz")
-# print(type(check_response_postcode.headers))
-#
-# # Lets see how we pass or get the data from dictionary
-# print (check_response_postcode.json()) # json() converts the data. there is a lot of data in the dict
-#
-# # Lets store this data into a variable
-# response_dict = check_response_postcode.json()
-# print(response_dict)
-# print(type(response_dict))
-#
-# # These 'result' in in the json dict
-# result_dict = response_dict['result']
-# print(response_dict)
-#
-# # iterating through the data
+import requests
+check_response_postcode = requests.get("https://api.postcodes.io/postcodes/wc1h8jz")
+
+
+#print(type(check_response_postcode.content)) # Type is bytes
+#print(type(check_response_postcode.headers)) # Type is requests.structures.CaseInsensitiveDict
+#print(type(check_response_postcode)) # Type is requests.models.Response
+
+
+# Lets see how we pass or get the data from dictionary
+
+response_dict = check_response_postcode.json() # json() converts the data into dict.
+
+#print(type(check_response_postcode.json())) # Type is dict
+#print(type(response_dict))
+
+
+
+# Lets store this data into a variable and iterate through it
+
+# The `response_dict` has another dict inside it, we want to access that
+# The 'result' dict is in the json dict
+
+result_dict = response_dict['result']
+print(result_dict)
+
 # for key in result_dict.keys():
 #     print(f"The name of the key is {key} and the value insider is {result_dict}")
 
@@ -85,22 +102,22 @@ import requests
 
 # Attempting to put everything in a class
 
-class Postcode:
-
-    def __init__(self,user_post):
-        self.user_post = user_post.replace(" ","")
-
-    check_response_postcode = requests.get(f"https://api.postcodes.io/postcodes/{self.user_post}")
-
-
-
-
-
-
-user_postcode = input("Please enter your postcode: ")
-postcode = Postcode(user_postcode)
-print(postcode.user_post)
-
+# class Postcode:
+#
+#     def __init__(self,user_post):
+#         self.user_post = user_post.replace(" ","")
+#
+#     check_response_postcode = requests.get(f"https://api.postcodes.io/postcodes/{self.user_post}")
+#
+#
+#
+#
+#
+#
+# user_postcode = input("Please enter your postcode: ")
+# postcode = Postcode(user_postcode)
+# print(postcode.user_post)
+#
 
 
 
